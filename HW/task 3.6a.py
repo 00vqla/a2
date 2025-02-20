9f4af411b85971497d8e45e8e7d542af10a1cc13
00vqla <00vqla@gmail.com>
2025-02-20 09:52:55 +0300

    2/20 a

--- HW/task 3.6a.py
+++ HW/task 3.6a.py
@@ -1,3 +1,19 @@
 ArrayNodes = []
 for i in range(0, 20):
     ArrayNodes.append([-1 for x in range(3)])
+
+ArrayNodes = (
+    [1, 20, 5], [2, 15, -1], [-1, 3, 3], [-1, 9, 4], [-1, 10, -1], [-1, 58, -1], [-1, -1, -1])
+FreeNode = 6
+RootPointer = 0
+
+
+# task 3.6b
+
+class node:
+    def __init__(self, data, nextNode):
+        self.data = data
+        self.nextNode = nextNode
+
+def addNode():
+
