f7019185c6f1a0e997c746e6bcc42572584b7c2a
00vqla <00vqla@gmail.com>
2025-02-21 09:23:34 +0300

    2/11

--- HW/task 3.6b.py
+++ HW/task 3.6b.py
@@ -3,4 +3,37 @@ class node:
         self.data = data
         self.nextNode = nextNode
 
-node 
+
+linkedList = [node(1,1), node(5, 4), node(6,7), node(7, -1), node(2,2), node(0, 6), node(0, 8), node(56, 3), node(0, 9), node(0, -1)]
+startPointer = 0
+emptyList = 5
+
+
+def outputNodes(linkedList, startPointer):
+    while startPointer != -1:
+        print(str(linkedList[startPointer].data))
+        startPointer = linkedList[startPointer].nextNode
+
+
+outputNodes(linkedList, startPointer)
+
+
+def addNode(linkedList, startPointer, emptyList):
+    newData = input("Enter the data to add: ")
+    if emptyList < 0 or emptyList > 9:
+        return False
+    else:
+        newNode = node(int(newData), -1)
+        linkedList[emptyList] = (newNode)
+
+        prevPointer = 0
+        while startPointer != -1:
+            prevPointer = startPointer
+            startPointer = linkedList[startPointer].nextNode
+        linkedList[prevPointer].nextNode = emptyList
+        emptyList = linkedList[emptyList].nextNode
+
+        return True
+
+addNode(linkedList, startPointer, emptyList)
+outputNodes(linkedList, startPointer)
