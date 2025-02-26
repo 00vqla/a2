2c6a88ad36177866009aa21b9c2982efafd02190
00vqla <00vqla@gmail.com>
2025-02-26 13:57:06 +0300

    2/26

--- HW/feb 25/Question2_N22.py
+++ HW/feb 25/Question2_N22.py
@@ -6,3 +6,31 @@ class Character:
         self.__Name = Name
         self.__XCoordinate = XCoordinate
         self.__YCoordinate = YCoordinate
+
+    def GetName(self):
+        return self.__Name
+
+    def GetX(self):
+        return self.__XCoordinate
+
+    def GetY(self):
+        return self.__YCoordinate
+
+    def ChangePosition(self, XChange, YChange):
+        self.__XCoordinate += XChange
+        self.__YCoordinate += YChange
+
+
+Characters = []
+TextFile = "Characters.txt"
+try:
+    File = open(TextFile, 'r')
+    for X in range(0, 10):
+        Name = File.readline().strip()
+        XCoord = File.readline().strip()
+        YCoord = File.readline().strip()
+        TempC = Character(Name, int(XCoord), int(YCoord))
+        Characters.append(TempC)
+    File.close()
+except:
+    print("File not found")
